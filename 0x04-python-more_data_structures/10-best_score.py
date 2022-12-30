549557d37598456818aaae289037e6e2a0a691ec
theodore <30gash@gmail.com>
2022-12-30 06:19:23 +0000

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        max_score = -1000
+        for key, value in a_dictionary.items():
+            if (value > max_score):
+                max_score = value
+                max_key = key
+        return max_key
