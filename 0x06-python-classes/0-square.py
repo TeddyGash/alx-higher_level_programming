2d18276cb4bba2202efa7876902d691588f13a64
theodore <30gash@gmail.com>
2023-01-01 11:36:15 +0000

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
 """This module contains the class Square for test purposes"""
 
 
 class Square():
     """Creates a blank Square class for example purposes only.
-    
     It has no attributes or methods.
-    
     """
     pass
