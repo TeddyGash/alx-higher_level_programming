39770fece78ed1eab080e0bb648f5f5f144b91ea
theodore <30gash@gmail.com>
2023-01-01 13:01:10 +0000

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,9 @@
 
 
 class Square():
-    """Creates a Square class for example purposes only and validates value of <size>.
-    """
+    """Creates a Square class for example purposes only and validates value of <size>."""
     def __init__(self, size=0):
         """Initializes a Class object with private attribute of __size.
-        
         Args:
             size(int): Size of square (must be a positive integer)
         """
