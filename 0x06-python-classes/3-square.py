cb976885be63b1b02a87301fb528391f4ac603f8
theodore <30gash@gmail.com>
2023-01-01 16:16:32 +0000

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Python test module for classes"""
+"""Python test module for classes."""
 
 
 class Square():
-    """Creates a Square class for example purposes only and validates value of <size>.
-    """
+    """Creates a Square class for example purposes only and validates value of <size>."""
+
     def __init__(self, size=0):
         """Initializes a Class object with private attribute of __size. 
         Args:
-            size(int): Size of square (must be a positive integer)
+            size(int): Size of square and must be a positive integer
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
