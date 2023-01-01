809cc7bbf74fd4447b0c21921b79e8ef3e7398f4
theodore <30gash@gmail.com>
2023-01-01 12:27:43 +0000

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python test module for classes"""
+
+
+class Square():
+    """Creates a Square class for example purposes only."""
+    def __init__(self, size):
+        """Initializes a Class object with private attribute of __size.
+        
+        Args:
+            size(int): Size of square
+        """
+        self.__size = size
