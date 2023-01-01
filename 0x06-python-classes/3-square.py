b80418b4387e1d0b50e8474083b25a07844f4df9
theodore <30gash@gmail.com>
2023-01-01 16:09:25 +0000

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,8 +6,7 @@ class Square():
     """Creates a Square class for example purposes only and validates value of <size>.
     """
     def __init__(self, size=0):
-        """Initializes a Class object with private attribute of __size.
-        
+        """Initializes a Class object with private attribute of __size. 
         Args:
             size(int): Size of square (must be a positive integer)
         """
